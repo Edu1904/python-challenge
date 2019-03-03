18ac1b77a9c65faa42efd4491b66ae66a8013009
Edu1904 <42618732+Edu1904@users.noreply.github.com>
2019-03-02 20:18:34 -0600

    main file is the script for Python Poll data HW

--- main.py
+++ main.py
@@ -2,117 +2,111 @@ import csv
 import os
 
 total_profit=0
-#open and read the file
-with open('py_bank.csv') as csvfile:
+
+with open('election_data.csv') as csvfile:
     readcsv = csv.reader(csvfile, delimiter=',')
- #def some variables
-    dates = []
-    profits = []
-    prof=0
-    total_profit=0
+    Voter_IDs = []
+    Countys = []
+    Candidates = []
+    
     header=next(readcsv)
 #    header=header.split(',')
 
-    changes=[]
-    change=0
-#populate profits and dates list
+#read the csv file and create separate list for 
+#each variable (voter_id,countys and candidates)   
     for row in readcsv:
-        profit = row[1]
-        date = row[0]
+        Voter_ID = row[0]
+        County = row[1]
+        Candidate= row[2]
 
-        dates.append(date)
-        profits.append(profit)
+        Voter_IDs.append(Voter_ID)
+        Countys.append(County)
+        Candidates.append(Candidate)
     
-        total_profit =int(total_profit) + int(profit)
-        change_profit=int(profit)- change
-        changes.append(change_profit)
-    
- # seting change to the current value, to calculate profit change of next value
-        change=int(profit)
+#def a function to count number of time each item in list appear
+def countCandidates (lst):
+    elems={}
+    for item in lst:
+        if item in elems.keys():
+            elems[item] +=1
+        else:
+            elems[item] = 1
+    return elems
 
- #   print(dates)
- #   print(profits)
- #   print (total_profit)
- #   print(changes[3])
+#count the number of time each candidate shows in candidates list, 
+#and create a dictionary
+votes_per_candidate ={}           
+votes_per_candidate = countCandidates(Candidates)
+#print (votes_per_candidate)
 
- #calculate 
-changeslenght=len(changes) 
-#print(changeslenght)
+#Calculate the number of vote cast
+total_votes=0
+total_votes = sum(votes_per_candidate.values())
+#print (total_votes)
 
-proflenght=len(profits)
-#print(len(profits))
+#calculate the maximun number of votes to determine the winner
+maxvote= max(votes_per_candidate.values())
 
-datelenght=len(dates)
-#print (datelenght)
+for key, value in votes_per_candidate.items():
 
-avgprofit=(total_profit)/(proflenght)
-#print(int(avgprofit))
+    if value == maxvote:
+        winner_name=(key)
+        winner_votes=(value)
+        winner_data=(key,value)
 
-changestotal=0
+votes_per_candidate_lenght=len(votes_per_candidate.keys())
+#print(votes_per_candidate)
+#print(votes_per_candidate_lenght)
 
-for item in changes:
+#print the output for all files show the elections results
+print (' ')
+print (' ')
+print ('Election Results')
+print (' ')
+print ('----------------------------- ')
+print (' ')
+print ('Total Votes:  '+ str(total_votes))
+print (' ')
+print ('----------------------------- ')
+print (' ')
+for key,value in votes_per_candidate.items():
+    print((key) + ':  ' +str(("%.3f" % round((value)*100/total_votes))) +'%'+ '  ('+str(value) +')')
+print (' ')
+print ('----------------------------- ')
+print (' ')
+print ('winner: '+str(winner_name))
+print (' ')
 
-    changestotal = changestotal + int(item)
+#create one string to make it easier to append to the text file
+total_v=('Total Votes:    '+ str(total_votes))
+winner_print=('Winner: '+str(winner_name))
 
-changestotal = changestotal - 867884
-
-changeavg = (changestotal)/(changeslenght-1)
-
-#print (changestotal)
-#print(changeavg)
-
-#calculate max and min change on profit find the date for each max and min value
-maximum = max (changes) 
-x= changes.index(max(changes))
-#print(dates[x])
-
-minimum = min (changes)
-y=changes.index(min(changes))
-#print(dates[y])
-#print(maximum)
-#print(minimum)
-fecha1=dates[x]
-fecha2=dates[y]
-
-#create print for results
-print(' ')
-print('Financial Analysis')
-print('----------------------------')
-print('Total Months:' + str(datelenght))
-print('Total: $'+str(total_profit))
-print('Average Change: $  ' + str(changeavg))
-print('Greatest Increase in Profits:  ' + str(fecha1) +' '+'($' + str(maximum) + ')')
-print('Greatest Decrease in Profits:  ' + str(fecha2) +' '+'($'+str(minimum)+')')
-
-#save strings variable to make easier to print on the output text file
-total_monthstext=('Total Months:' + str(datelenght))
-total_profittext=('Total: $'+str(total_profit))
-avg_changetext=('Average Change: $  ' + str(changeavg))
-greatest_inctext=('Greatest Increase in Profits:  ' + str(fecha1) +' '+'($' + str(maximum) + ')')
-greates_dectext=('Greatest Decrease in Profits:  ' + str(fecha2) +' '+'($'+str(minimum)+')')
-
-
-#output a text file with the results
-f1 = open ("Py_bank_result.txt","w")
+#create the text file with the same data that print from the script
+f1= open('file_test.txt','w')
 f1.write(' \n')
 f1.write(' \n')
-f1.write('Financial Analysis\n')
+f1.write('Election Results\n')
 f1.write(' \n')
 f1.write('----------------------------- \n')
 f1.write(' \n')
-f1.write(total_monthstext)
+f1.write(total_v)
+f1.close()
+
+f1= open('file_test.txt','a')
 f1.write(' \n')
 f1.write(' \n')
-f1.write(total_profittext)
+f1.write('----------------------------- \n')
 f1.write(' \n')
+f1.close()
+for key,value in votes_per_candidate.items():
+    output=((key) + ':  ' +str(("%.3f" % round((value)*100/total_votes))) +'%'+ '  ('+str(value) +')')
+    f1= open('file_test.txt','a')
+    f1.write(output)
+    f1.write(' \n')
+    f1.close()
+f1= open('file_test.txt','a')
 f1.write(' \n')
-f1.write(avg_changetext)
+f1.write('----------------------------- \n')
 f1.write(' \n')
-f1.write(' \n')
-f1.write(greatest_inctext)
-f1.write(' \n')
-f1.write(' \n')
-f1.write(greates_dectext)
-f1.write(' \n')
-f1.write(' \n')
+f1.write(winner_print)
 f1.close()
